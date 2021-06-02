af1f5cb6f4337ab09f3072785f07ddf154b9244f
Alex Dem <a.dziamenchanka@itechart-group.com>
2021-06-02 13:42:55 +0300

    Styled default application layout with semantic ui

--- app/javascript/packs/semantic-ui.js
+++ app/javascript/packs/semantic-ui.js
@@ -10,4 +10,35 @@ document.addEventListener("turbolinks:load", function() {
       ;
     })
   ;
+
+  // fix main menu to page on passing
+  $('.main.menu').visibility({
+    type: 'fixed'
+  });
+
+  $('.overlay').visibility({
+    type: 'fixed',
+    offset: 80
+  });
+
+  // lazy load images
+  $('.image').visibility({
+    type: 'image',
+    transition: 'vertical flip in',
+    duration: 500
+  });
+
+  // show dropdown on hover
+  $('.main.menu  .ui.dropdown').dropdown({
+    on: 'hover'
+  });
+
+  // modal
+  $('.modal').modal('setting', 'transition', 'vertical flip')
+
+  $('.modal-trigger').each(function() {
+    $(this).on("click", function() {
+      $(this.dataset.modalTarget).modal('show')
+    });
+  });
 });
